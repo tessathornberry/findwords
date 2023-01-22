cb929849e00546be48a404462054e9081c781459
Tessa Thornberry <tthornberryclass@gmail.com>
2023-01-21 18:37:48 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppBorder = styled.div`
+border: 1px solid gray;
+border-radius: 10px;
+padding: 1em;
+`;
+
+const App = () => {
+
+  return (
+    <AppBorder>
+    <div>Find Words</div>
+
+
+    </AppBorder>
+  )
+};
+
+export default App;
